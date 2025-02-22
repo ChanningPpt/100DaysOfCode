c15f8c7347ed59c6d0c57737b7aee095185c1191
Channing Ppt <channing.phong@gmail.com>
2025-02-22 02:45:23 -0500

    Update

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -28,5 +28,63 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0
 }
 
+def insert_coins():
+    print("Please insert coins.")
+    quarters_amount = int(input("How many quarters?: "))
+    dimes_amount = int(input("How many dimes?: "))
+    nickles_amount = int(input("How many nickles?: "))
+    pennies_amount = int(input("How many pennies?: "))
+
+    total_amount = (quarters_amount * 0.25) + (dimes_amount * 0.10) + (nickles_amount * 0.05) + (pennies_amount * 0.01)
+
+    return total_amount
+
+coffee_machine_status = True
+
+while coffee_machine_status:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if user_input == "espresso".lower():
+        if resources["water"] >= MENU["espresso"]["ingredients"]["water"] and resources["coffee"] >= MENU["espresso"]["ingredients"]["coffee"]:
+            inserted_money = insert_coins()
+            if inserted_money >= MENU["espresso"]["cost"]:
+                resources["money"] += MENU["espresso"]["cost"]
+
+                resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+                resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+
+                if inserted_money > MENU["espresso"]["cost"]:
+                    change = inserted_money - MENU["espresso"]["cost"]
+                    print(f"Here is ${change:.2f} dollars in change.")
+
+                print("Here is your espresso. Enjoy!")
+            else:
+                print("Sorry, that's not enough money. Money refunded.")
+
+        elif resources["water"] < MENU["espresso"]["ingredients"]["water"]:
+            print("Sorry, there is not enough water.")
+        elif resources["coffee"] < MENU["espresso"]["ingredients"]["coffee"]:
+            print("Sorry, there is not enough coffee.")
+
+
+
+    elif user_input == "latte".lower():
+        pass
+
+
+
+    elif user_input == "cappuccino".lower():
+        pass
+
+
+
+    elif user_input == "report".lower():
+        print(f"Water: {resources["water"]}ml" + "\n" + f"Milk: {resources["milk"]}ml" + "\n" + f"Coffee: {resources["coffee"]}g" + "\n" + f"Money: ${resources["money"]}")
+
+
+
+    elif user_input == "off".lower():
+        break
